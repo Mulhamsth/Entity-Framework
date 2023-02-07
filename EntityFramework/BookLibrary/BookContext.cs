0e4defe259d6bab42beb374689c62ea589472ff9
Mulhamsth <mulhamsth@gmail.com>
2023-02-07 21:31:14 +0100

    creating database and tables

---
+++ EntityFramework/BookLibrary/BookContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
