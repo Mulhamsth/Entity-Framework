1e7c779a35b64fea109847e292193825c771b077
Mulhamsth <mulhamsth@gmail.com>
2023-02-10 22:31:39 +0100

    adding asp web app

--- EntityFramework/BookLibrary/BookContext.cs
+++ EntityFramework/BookLibrary/BookContext.cs
@@ -9,7 +9,12 @@ namespace BookLibrary
 {
     public class BookContext : DbContext
     {
+        public BookContext(){ }
         public BookContext(DbContextOptions options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=../../../MyLocalLibrary.db");
+        }
 
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
